5727e20df4a820822b56489f3d0994dd1b90e365
riuga <mosks@bk.ru>
2024-07-31 03:19:02 +0300

    add modal for users

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-// import './UserModal.css'
+import './Modal.scss'
 
 const Modal = ({ user, close }) => {
   return (
     <div className='modal'>
-      <h3>{`${user.firstName} ${user.maidenName} ${user.lastName}`}</h3>
-      <p>Age: {user.age}</p>
-      <p>Address: {`${user.address.address}, ${user.address.city}`}</p>
-      <p>Height: {user.height}</p>
-      <p>Weight: {user.weight}</p>
-      <p>Phone: {user.phone}</p>
-      <p>Email: {user.email}</p>
-      <button onClick={close}>Close</button>
+      <div className='modal-content'>
+        <h3>{`${user.firstName} ${user.maidenName} ${user.lastName}`}</h3>
+        <p>Age: {user.age}</p>
+        <p>Address: {`${user.address.address}, ${user.address.city}`}</p>
+        <p>Height: {user.height}</p>
+        <p>Weight: {user.weight}</p>
+        <p>Phone: {user.phone}</p>
+        <p>Email: {user.email}</p>
+        <button onClick={close}>Close</button>
+      </div>
     </div>
   )
 }
